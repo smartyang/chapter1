6d9be65980ed60582cc72c14b2b55fc43324936e
smartyang <hlbryang@gmail.com>
2013-05-24 17:42:57 +0800

    first time update a file in github

---
+++ common.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Statue;
+typedef int Bool;
+
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
